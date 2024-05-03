6017dea0627dd5f26eabb81f61f331be89cc5bd8
runjuu <hi@runjuu.com>
2024-05-03 14:23:55 -0700

    refactor: rename apps

---
+++ apps/website/app/page.tsx
@@ -0,0 +1,9 @@
+import { HELLO } from "@seed-sdk/types";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {HELLO}
+    </main>
+  );
+}
